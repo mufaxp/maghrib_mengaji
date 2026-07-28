0ade96185ccfd3019172f95f8e4780fba4037d12
Mufadilah Hermansyah <mfdilah@gmail.com>
2026-07-28 09:53:08 +0700

    v0.14.10 - Perbaikan sederhana

--- helpers/mediaHelper.js
+++ helpers/mediaHelper.js
@@ -95,7 +95,8 @@ export async function processVoiceFromBuffer(buffer, className, studentName) {
   const safeClassName = sanitizeString(className);
   const safeStudentName = sanitizeString(studentName);
   const timestamp = Date.now();
-  const fileName = `${safeClassName}_${safeStudentName}_${timestamp}.ogg`;
+  const ext = '.webm';
+  const fileName = `${safeClassName}_${safeStudentName}_${timestamp}${ext}`;
 
   await saveFile(fileName, buffer);
   return fileName;
